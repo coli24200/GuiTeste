0f96f122873495980e4d14ef0dcaf4f609c3f278
COliveira <coli24200@gmail.com>
2021-10-29 11:08:40 +0100

    commit1

---
+++ src/com/oliveira/RunCO.java
@@ -0,0 +1,16 @@
+package com.oliveira;
+
+import javax.swing.*;
+
+public class RunCO {
+    public static void main(String[] args) {
+        new CO_Frame();
+    }
+
+    public static class CO_Frame {
+        private JButton button1;
+        private JButton button2;
+        private JButton button3;
+        private JButton button4;
+    }
+}
